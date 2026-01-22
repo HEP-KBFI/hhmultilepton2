d856a1eb9eb9dc5a1b1a2cf557ff69007802cd05
Khawla Jaffel <khawla.jaffel@cern.ch>
2026-01-22 12:54:21 +0200

    need to skip categories for certain channels when they are skipped

--- multilepton/categorization/default.py
+++ multilepton/categorization/default.py
@@ -147,17 +147,17 @@ def cat_4tau(self: Categorizer, events: ak.Array, **kwargs) -> tuple[ak.Array, a
 
 
 @categorizer(uses={"channel_id"})
-def cat_c2e0or1tau(self: Categorizer, events: ak.Array, **kwargs) -> tuple[ak.Array, ak.Array]:
+def cat_2e0or1tau(self: Categorizer, events: ak.Array, **kwargs) -> tuple[ak.Array, ak.Array]:
     return events, events.channel_id == self.config_inst.channels.n.c2e0or1tau.id
 
 
 @categorizer(uses={"channel_id"})
-def cat_cemu0or1tau(self: Categorizer, events: ak.Array, **kwargs) -> tuple[ak.Array, ak.Array]:
+def cat_emu0or1tau(self: Categorizer, events: ak.Array, **kwargs) -> tuple[ak.Array, ak.Array]:
     return events, events.channel_id == self.config_inst.channels.n.cemu0or1tau.id
 
 
 @categorizer(uses={"channel_id"})
-def cat_c2mu0or1tau(self: Categorizer, events: ak.Array, **kwargs) -> tuple[ak.Array, ak.Array]:
+def cat_2mu0or1tau(self: Categorizer, events: ak.Array, **kwargs) -> tuple[ak.Array, ak.Array]:
     return events, events.channel_id == self.config_inst.channels.n.c2mu0or1tau.id
 
 
